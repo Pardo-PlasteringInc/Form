8fd40839bfaee8b08e2e7a428d78f5c8c1d39279
JPE <josepardo69@yahoo.com>
2019-12-03 20:02:06 -0800

    FomularioEmpleados

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$con=mysqli_connect('localhost','root','','FormularioEmpleados') or die ('error conecion con servidor');
+$sql="INSERT INTO bbdatos VALUES('".$_POST["name"]."','".$_POST["email"]."','".$_POST["message"]."')";
+$resultado=mysqli_query($con,$sql) or die('Error en query');
+mysqli_close($con);
+    if($resultado){
+        header("Location: index.html");
+    }
+    else{
+        echo "Error sending message";
+    }
+}
